5531771d9f23216bba83532ed0ad5a98886ad3ab
Reid <rmcc0724@gmail.com>
2019-01-27 01:36:35 +0000

    Adding posts and custom classes

--- wp-content/themes/girlsingerjoyce/header.php
+++ wp-content/themes/girlsingerjoyce/header.php
@@ -5,10 +5,33 @@
 <meta charset="utf8">
 <title>Girl Singer Joyce</title>
 
-<!--here we add a function call to the header using the wp_head() so it can be included in other files -->
-<?php wp_head(); ?>
+
+<?php wp_head(); 
+
+/*
+============================================================
+-here we add a function call to the header using the wp_head() so it can be included in other files
+============================================================
+*/
+
+?>
+
 </head>
-	<body>  
+
+<?php 
+/*
+============================================================
+This function checks to see if the page is true : it uses the basictheme-class else no-basictheme-class
+============================================================
+*/
+		
+		if( is_front_page() ):
+			$basictheme_classes = array( 'basictheme-class', 'my-class' );
+		else:
+			$basictheme_classes = array( 'no-basictheme-class' );
+		endif;
+	?>
+<body <?php body_class( $basictheme_classes ); ?>>
 	<h1>Here's the header</h1>
 	
   <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
--- wp-content/themes/girlsingerjoyce/index.php
+++ wp-content/themes/girlsingerjoyce/index.php
@@ -1,4 +1,19 @@
 <?php get_header(); ?>
+<?php
+/*
+============================================================
+If there are post, print all of the post content 
+============================================================
+*/
+if(have_posts() ):
+    while( have_posts() ): the_post(); ?>
+    
+    <h3><?php the_title(); ?></h3>
+    <p><?php the_content(); ?></p>
+    
+<?php endwhile;
+endif;
+?>
+
 <h1>This is my theme</h1>
 <?php get_footer(); ?>
-
